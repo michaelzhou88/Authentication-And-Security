96c324ecac3d64f5b84ca696e469ed5518f442d3
michaelzhou88 <38016799+michaelzhou88@users.noreply.github.com>
2020-08-12 22:51:45 +0100

    Level 6 OAuth2 Sign in with Google Account

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const findOrCreatePlugin = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -28,48 +31,122 @@ app.use(passport.initialize());
 // Use passport to deal with the sessions
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 // Setup passport-local-mongoose package as a plugin with a mongoose schema
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-// Serialise - Creates a 'fortune cookie' and stuffs the message (e.g.user's ID)
-passport.serializeUser(User.serializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
-// Deserialise - Allows passport to 'crumble the cookie' and decrypt the message
-passport.deserializeUser(User.deserializeUser());
-
-
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req,res){
     res.render("home");
 });
 
-app.get("/login", function(req,res){
+// Once this route is hit up upon 'Sign up with Google' card is pressed
+app.get("/auth/google", 
+    // Initiate authentication on google servers asking for user's profile once they have logged in
+    passport.authenticate("google", {scope: ["profile"] })
+);
+
+// Assuming sign in is successful 
+app.get("/auth/google/secrets", 
+  // Want user's google profile
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res){
+    // Successful authentication, redirect to secrets
+    res.redirect("/secrets");
+  });
+
+
+app.get("/login", function(req, res){
     res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+app.get("/secrets", function(req, res){	
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){	
+      if (err){	
+        console.log(err);	
+      } else {	
+        if (foundUsers) {	
+          res.render("secrets", {usersWithSecrets: foundUsers});	
+        }	
+      }	
+    });	
 });
+
+app.get("/submit", function(req, res){
+    if (req.isAuthenticated()){
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+  
+  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+    // console.log(req.user.id);
+  
+    User.findById(req.user.id, function(err, foundUser){
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+  });
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
 
 app.post("/register", function(req, res){
 
@@ -85,12 +162,11 @@ app.post("/register", function(req, res){
             });
         }
     });
+
 });
 
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});
+
+
 
 app.post("/login", function(req, res){
     const user = new User({
@@ -109,6 +185,12 @@ app.post("/login", function(req, res){
     });
 });
 
-app.listen(3000, function(){
+
+
+
+
+
+
+app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
